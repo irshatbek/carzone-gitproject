4c9b207b9b3aacf0fd7251ae11474f0f4ce1ad20
irshatbek <irshatbek@gmail.com>
2022-05-11 18:53:21 +0600

    modified register login and logout pages

--- carzone/urls.py
+++ carzone/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('cars/', include('cars.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
