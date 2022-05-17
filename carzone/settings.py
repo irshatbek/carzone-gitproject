fae3ab791b2ad00b5795bf7bef742d335e1d521a
irshatbek <irshatbek@gmail.com>
2022-05-18 05:53:31 +0600

    try one

--- carzone/settings.py
+++ carzone/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '934nw3r62@!m0^ksgw3#31tntglnr%td+-_b89xpu2@q2zqv=d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['floating-badlands-41165.herokuapp.com', 'carzoneapp.co', 'www.carzoneapp.co']
+ALLOWED_HOSTS = ['*',]
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
